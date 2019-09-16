93aa074894485be6efc89d68af93c117099310b1
jason <jason.addie@gmail.com>
2019-09-16 20:26:54 +0400

    bug fix - rake task name in schedlue

--- config/schedule.rb
+++ config/schedule.rb
@@ -2,7 +2,7 @@ job_type :run_rake_task, 'cd :path && bundle exec rake :task --quiet'
 
 # scrape last months data
 every '0 20 14 * *' do
-  run_rake_task 'scraper:main_scrape_tasks:last_month'
+  run_rake_task 'scraper:main_scrape_tasks:previous_month'
 end
 
 # update the proxies every 30 mins when the scraper is running
