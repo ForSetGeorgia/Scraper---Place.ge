59a99199625873899fa1b0d4fdc9fa671420836c
jason <jason.addie@gmail.com>
2019-09-20 12:34:46 +0400

    update cronjob schedule

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,11 +1,11 @@
 job_type :run_rake_task, 'cd :path && bundle exec rake :task --quiet'
 
 # scrape last months data
-every '0 20 14 * *' do
+every '0 01 14 * *' do
   run_rake_task 'scraper:main_scrape_tasks:previous_month'
 end
 
 # update the proxies every 30 mins when the scraper is running
-every '*/30 * 14,15 * *' do
+every '*/30 * 14,15,16,17,18 * *' do
   run_rake_task 'scraper:update_proxy_list'
 end
